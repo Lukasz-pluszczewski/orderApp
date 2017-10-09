41df73c1912a1498794e530c4da550ee2b0af36e
lukasz_brainhub <lukasz@brainhub.pl>
2017-10-09 10:58:24 +0200

    Fixed router

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-
-import createHistory from 'history/createBrowserHistory';
-import { ConnectedRouter } from 'react-router-redux';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import App from 'containers/App';
 import OrderPage from 'pages/OrderPage';
 
 export default (
-  <ConnectedRouter history={createHistory()}>
+  <BrowserRouter>
     <App>
       <Switch>
         <Route exact path="/" component={OrderPage}/>
       </Switch>
     </App>
-  </ConnectedRouter>
+  </BrowserRouter>
 );
