c327cb59b8100dfed51cfb2edcd094851e15249a
lukasz_brainhub <lukasz@brainhub.pl>
2017-10-09 10:46:49 +0200

    Removed unneeded code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
-/* eslint-disable import/first */
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 
 import routes from './routes';
-import configureStore from './store/configureStore';
 
 import './favicon.png';
 import './styles/styles.scss';
 
-const store = configureStore();
-
-// translations
-import { setLanguage } from 'mi18n-redux';
-import en from 'constants/translations/en';
-store.dispatch(setLanguage('en', en));
-
 render(
-  <Provider store={store}>
-    {routes}
-  </Provider>,
+  routes,
   document.getElementById('app')
 );
