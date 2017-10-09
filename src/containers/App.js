b86fb0fba2eee60bba6a1942f9b7c577e0f0ad56
lukasz_brainhub <lukasz@brainhub.pl>
2017-10-09 10:51:29 +0200

    Removed unneeded code #2

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,13 @@
 import { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
-import connectSocket from 'services/socket';
 
-import { internetConnected, internetDisconnected, serverConnected, serverDisconnected } from 'actions/statusActions';
-
-class App extends Component {
+export default class App extends Component {
   static propTypes = {
     // props
     children: PropTypes.element,
-
-    // redux actions
-    internetConnected: PropTypes.func,
-    internetDisconnected: PropTypes.func,
-    serverConnected: PropTypes.func,
-    serverDisconnected: PropTypes.func,
   };
-
-  componentWillMount() {
-    connectSocket({
-      onConnect: this.props.serverConnected,
-      onDisconnect: this.props.serverDisconnected,
-    });
-    window.addEventListener('online',  this.props.internetConnected);
-    window.addEventListener('offline', this.props.internetDisconnected);
-  }
 
   render() {
     return this.props.children;
   }
 }
-
-export default withRouter(connect(
-  null,
-  {
-    internetConnected,
-    internetDisconnected,
-    serverConnected,
-    serverDisconnected,
-  }
-)(App));
